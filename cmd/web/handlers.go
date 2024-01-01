2a74e630a2e6e8971596418717f12d5cc7b1d825
jacob <>
2024-01-01 13:53:28 +0100

    chapter 5.5: common dynamic data

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -19,10 +19,12 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 		app.serverError(w, err)
 		return
 	}
+
+	data := app.newTemplateData(r)
+	data.Snippets = snippets
+
 	// Use the new render helper.
-	app.render(w, http.StatusOK, "home.tmpl", &templateData{
-		Snippets: snippets,
-	})
+	app.render(w, http.StatusOK, "home.tmpl", data)
 }
 
 func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
@@ -62,8 +64,9 @@ func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 		}
 		return
 	}
+
+	data := app.newTemplateData(r)
+	data.Snippet = snippet
 	// Use the new render helper.
-	app.render(w, http.StatusOK, "view.tmpl", &templateData{
-		Snippet: snippet,
-	})
+	app.render(w, http.StatusOK, "view.tmpl", data)
 }
