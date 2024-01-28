20cca1b25e56b5e144822aca4a37204b1021b369
jacob <>
2024-01-28 14:05:10 +0100

    chapter 14.5: mock dependencies

--- internal/models/users.go
+++ internal/models/users.go
@@ -25,6 +25,12 @@ type UserModel struct {
 	DB *sql.DB
 }
 
+type UserModelInterface interface {
+	Insert(name, email, password string) error
+	Authenticate(email, password string) (int, error)
+	Exists(id int) (bool, error)
+}
+
 // We'll use the Insert method to add a new record to the "users" table.
 func (m *UserModel) Insert(name, email, password string) error {
 	// Create a bcrypt hash of the plain-text password.
